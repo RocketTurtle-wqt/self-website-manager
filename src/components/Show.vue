0a54d2fbebcfe66064ff0fc9d984c6a35fdd36d4
wqt <1600639146@qq.com>
2021-03-18 11:52:41 +0800

    解决文章回显样式

--- src/components/Show.vue
+++ src/components/Show.vue
@@ -2,13 +2,15 @@
   <div id="show">
     <el-collapse v-model="activeNames" @change="handleChange">
       <el-collapse-item v-for="(item, index) in this.$store.state.selectClassify" :title="item.title" :name="index" :key="item.classify_id">
-        <main v-html="item.artical"></main>
+        <main v-html="item.artical" class="markdown-body"></main>
       </el-collapse-item>
     </el-collapse>
   </div>
 </template>
 
 <script>
+import "mavon-editor/dist/css/index.css";
+
 export default {
   name:"show",
     data() {
--- src/config/net.js
+++ src/config/net.js
@@ -1,4 +1,4 @@
-let server = 'http://10.8.157.62:7002';
+let server = 'http://192.168.3.28:7002';
 
 export {
   server
